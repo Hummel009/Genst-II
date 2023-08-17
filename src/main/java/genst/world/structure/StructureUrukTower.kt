b65ecad133699fa5231dcde9d7c0d57f067f479a
Jahor Burboŭski <60618853+Hummel009@users.noreply.github.com>
2023-08-17 12:12:26 +0300

    лучше

--- src/main/java/genst/world/structure/StructureUrukTower.kt
+++ src/main/java/genst/world/structure/StructureUrukTower.kt
@@ -6,7 +6,7 @@ import net.minecraft.world.World
 import java.util.*
 
 object StructureUrukTower : StructureTowerBase() {
-	override fun getSections(): Int = 4
+	override fun getSections(): Int = 7
 
 	override fun placeWall(world: World, i: Int, j: Int, k: Int) {
 		setBlockAndMetadata(world, i, j, k, LOTRMod.wall2, 7)
