e8143fa98e7cd26e73e1f3be750be8751208ece6
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-11-22 23:19:03 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
-import org.jetbrains.gradle.ext.Gradle
-import org.jetbrains.gradle.ext.RunConfigurationContainer
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.9.20"
-	id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
 	id("com.gtnewhorizons.retrofuturagradle") version "1.3.24"
 }
 
@@ -21,35 +18,13 @@ dependencies {
 
 java {
 	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(8))
+		languageVersion = JavaLanguageVersion.of(8)
 	}
 }
 
 minecraft {
 	mcVersion.set("1.7.10")
 	username.set("Hummel009")
-}
-
-idea {
-	project {
-		this.withGroovyBuilder {
-			"settings" {
-				"runConfigurations" {
-					val self = this.delegate as RunConfigurationContainer
-					self.add(
-						Gradle("1. Run Client").apply {
-							setProperty("taskNames", listOf("runClient"))
-						},
-					)
-					self.add(
-						Gradle("2. Run Server").apply {
-							setProperty("taskNames", listOf("runServer"))
-						},
-					)
-				}
-			}
-		}
-	}
 }
 
 tasks {
