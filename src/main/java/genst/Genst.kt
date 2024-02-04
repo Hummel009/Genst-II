ad0b7c6393467b22857c37bca6abf0cf2be1819a
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-04 22:37:19 +0300

    no need

--- src/main/java/genst/Genst.kt
+++ src/main/java/genst/Genst.kt
@@ -1,10 +1,8 @@
 package genst
 
 import cpw.mods.fml.common.Mod
-import cpw.mods.fml.common.SidedProxy
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
-import genst.proxy.CommonProxy
 import genst.world.GenstLocations
 import genst.world.GenstMountains
 import genst.world.GenstRoads
@@ -13,18 +11,9 @@ import genst.world.GenstWaypoints
 @Mod(modid = "genst", dependencies = "required-after:lotr", useMetadata = true)
 @Suppress("unused", "UNUSED_PARAMETER")
 class Genst {
-	companion object {
-		@SidedProxy(clientSide = "genst.proxy.ClientProxy", serverSide = "genst.proxy.CommonProxy")
-		@JvmStatic
-		lateinit var proxy: CommonProxy
-
-		const val VERSION: String = "24.02.29"
-	}
-
 	@Mod.EventHandler
 	fun preInit(e: FMLPreInitializationEvent) {
 		GenstWaypoints.preInit()
-		proxy.preInit()
 	}
 
 	@Mod.EventHandler
--- src/main/java/genst/proxy/CommonProxy.kt
+++
@@ -1,5 +0,0 @@
-package genst.proxy
-
-open class CommonProxy {
-	open fun preInit() {}
-}
